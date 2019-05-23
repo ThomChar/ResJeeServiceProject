16cd9ab60856815a54ea10fa3468343c483e5f4b
ThomChar <redtiger49@laposte.net>
2019-05-23 18:14:50 +0200

    projet werveur Soap Quasiment fonctionnel

--- ProjetSoap/InstaWebSoap/src/com/javahelps/helloaxis/HelloService.java
+++ ProjetSoap/InstaWebSoap/src/com/javahelps/helloaxis/HelloService.java
@@ -1,9 +1,26 @@
 package com.javahelps.helloaxis;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import com.instablog.connexion.InstaException;
+//import com.instablog.connexion.EMFManager;
+import com.instablog.dao.UserDao;
+import com.instablog.model.User;
+
 public class HelloService {
 
+	UserDao userDao = new UserDao();
+	
 	public String sayHello(String name) {
+		//EntityManager em = EMFManager.getEntityManager(); 
         return "Hello " + name;
+    }
+	
+	public List<User> users(String name) throws InstaException {
+		//EntityManager em = EMFManager.getEntityManager(); 
+        return userDao.getAllUsers();
     }
 	
 }
